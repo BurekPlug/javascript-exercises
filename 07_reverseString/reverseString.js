811798dc5f2eac0de1cf50a07e48527f67031eba
BurekPlug <198450603+BurekPlug@users.noreply.github.com>
2025-08-23 15:55:17 +0200

    07: my solution

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    const inputArr = input.split("");
+    let resultString = "";
+    for (let idx = (input.length-1); idx>=0; idx--)
+    {
+        resultString += inputArr[idx];
+    }
+    return resultString;
 };
 
 // Do not edit below this line
