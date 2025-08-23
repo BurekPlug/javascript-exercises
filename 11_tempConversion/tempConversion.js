4932d1d62de0a71b71773096e1d68e34ba28f383
BurekPlug <198450603+BurekPlug@users.noreply.github.com>
2025-08-23 17:30:28 +0200

    11: my solution

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,10 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(FH) {
+  return Math.round((FH - 32) * (5/9) * 10) / 10;
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(Ce) {
+  return Math.round((Ce * 9/5 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
