972185d4f086fa2702be647ae9941e0f23e1566e
BurekPlug <198450603+BurekPlug@users.noreply.github.com>
2025-08-23 17:10:11 +0200

    09: my solution

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(fst, snd) {
+    
+    var lowBound, uppBound;
+    
+    if(fst>snd)
+    {
+        uppBound = fst;
+        lowBound = snd;
+    }
+    else
+    {
+        uppBound = snd;
+        lowBound = fst;
+    }
+    
+    if (lowBound < 0 || uppBound < 0 || !Number.isInteger(lowBound)|| !Number.isInteger(uppBound)) return "ERROR";
+    
+    var sum = 0;
+    for (let i = lowBound; i<=uppBound; i++)
+    {
+    sum += i;
+    }
+    
 
+    return sum;
 };
 
 // Do not edit below this line
