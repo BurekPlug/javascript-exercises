f457adef6768306b4686da64c061f5fef251880d
BurekPlug <198450603+BurekPlug@users.noreply.github.com>
2025-08-23 16:48:36 +0200

    08: my solution

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    let resArr=arr.slice();
+    for (let arg of args)
+    {
+        resArr = resArr.filter( item => item !== arg);
+    }
+    
+    return resArr;
 };
 
 // Do not edit below this line
