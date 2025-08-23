1fc67b07d38164cc7a8a80e7319bb5ada99c4f5f
BurekPlug <198450603+BurekPlug@users.noreply.github.com>
2025-08-23 13:20:15 +0200

    06: my solution

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    resultstring ="";
+    for (let i = 0; i<num; i++) {
+        resultstring = resultstring.concat(string);
+    }
+    return resultstring;
 };
 
 // Do not edit below this line
