97382f294848b1460a0201b5ae4b3efc0faa7df2
BurekPlug <198450603+BurekPlug@users.noreply.github.com>
2025-09-27 16:49:28 +0200

    15: my solution

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(persons) {
+    //add age
+    persons.map(setAge);
+    persons = persons.sort((a,b) => b.age - a.age);
+    return persons[0]
 };
+
+function setAge(val) {
+    if (val.yearOfDeath === undefined)
+        val.age = (new Date().getFullYear()) - val.yearOfBirth
+    else
+    {
+        val.age = val.age = val.yearOfDeath - val.yearOfBirth;
+    }
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
