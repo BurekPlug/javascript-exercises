d9981a61781450340ece5e670cbf05a62ad81ca8
BurekPlug <198450603+BurekPlug@users.noreply.github.com>
2025-09-27 10:57:05 +0200

    13: my solution

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,38 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    //create a single string with pure chars
+    partStr = text.split(" ")
+    let singString = partStr.join("");
 
+    partStr = singString.split("!")
+    singString = partStr.join("");
+
+
+    partStr = singString.split("?")
+    singString = partStr.join("");
+
+    partStr = singString.split(",")
+    singString = partStr.join("");
+
+    
+    partStr = singString.split(".")
+    singString = partStr.join("");
+
+
+    singString = singString.toUpperCase();
+
+    //test the string
+    for (let i = 0; i < (singString.length/2-1); i++)
+    {
+
+        if (singString[i] === singString[singString.length-1-i])
+            {console.log("continue");
+            continue;}
+        else
+            {console.log("false");
+              return false;}
+    }
+
+    return true;
 };
 
 // Do not edit below this line
