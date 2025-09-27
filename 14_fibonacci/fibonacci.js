af92517fcc38ac55994e70488e0240760ad43cb5
BurekPlug <198450603+BurekPlug@users.noreply.github.com>
2025-09-27 11:37:58 +0200

    14: my solution

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+    let sum = 0;
+    fiboArr = [ 0, 1]
 
+    if(typeof a === "string")
+        a = Number(a)
+
+    if (Number.isInteger(a))
+    {
+        if(a<0)
+            return ("OOPS");
+        
+        switch (a) {
+            case 0:
+                return 0;
+            case 1:
+                return 1
+            default:
+                for (let i = 2; i <= a; i++) {
+                    fiboArr.push(fiboArr[i-2] + fiboArr[i-1]);
+                }
+                return fiboArr[a];
+        }
+
+    }
 };
 
 // Do not edit below this line
